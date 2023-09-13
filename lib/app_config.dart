d1a6cfbe9b64af4e5b1db9086fe33b21dcdb037c
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2023-09-13 11:26:49 +0600

    app config

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -4,9 +4,14 @@ class AppConfig{
   static String NYC_POEN_CAMERA_VOILATION = "https://data.cityofnewyork.us/resource/nc67-uf89.json?plate=";
 
 
+  static String DOMAIN = "https://alloneautos.com";
   static String BASE_URL = "https://alloneautos.com/api";
   static String OFFERS = "$BASE_URL/pricings";
   static String LOGIN = "$BASE_URL/vendor/login";
   static String DASHBOARD = "$BASE_URL/vendor/dashboard";
   static String RENT_CAR_LIST = "$BASE_URL/vendor/rent/cars";
+  static String DELETE_RENT_CAR = "$BASE_URL/vendor/rent/car/"; //END POINT IS SHARED
+  static String ADD_CAR_FOR_RENT = "$BASE_URL/vendor/rent/car";
+  static String UPDATE_RENT_CAR = "$BASE_URL/vendor/rent/car/15/update"; //END POINT IS SHARED
+  static String SINGLE_RENT_CAR = "$BASE_URL/vendor/rent/car/"; //END POINT IS SHARED
 }
