2833568691e7883759ee0e8a55e44a50be5a6288
Nayon Talukder <69708059+nayon-coders@users.noreply.github.com>
2023-12-12 14:57:32 +0600

    ap button

--- lib/view_controller/appButton.dart
+++ lib/view_controller/appButton.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../utility/app_color.dart';
 
 class AppButton extends StatelessWidget {
   const AppButton({
-    Key? key, required this.onClick, required this.text,  this.bgColor = AppColors.green,  this.borderColor=Colors.transparent, required this.width,  this.height=40.00,  this.textSize= 13.00, this.textColor = Colors.white
+    Key? key, this.isLoading = false, required this.onClick, required this.text,  this.bgColor = AppColors.green,  this.borderColor=Colors.transparent, required this.width,  this.height=40.00,  this.textSize= 13.00, this.textColor = Colors.white
   }) : super(key: key);
   final String text;
   final Color bgColor;
@@ -14,6 +15,7 @@ class AppButton extends StatelessWidget {
   final double height;
   final double textSize;
   final VoidCallback onClick;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,11 @@ class AppButton extends StatelessWidget {
             border: Border.all(width: 1, color: borderColor)
         ),
         child: Center(
-          child: Text("$text",
+          child: isLoading
+              ? SpinKitWave(
+            color: Colors.white,
+            size: 20.0,
+          ) : Text("$text",
             style: TextStyle(
                 fontSize: textSize,
                 fontWeight: FontWeight.w600,
@@ -40,3 +46,38 @@ class AppButton extends StatelessWidget {
     );
   }
 }
+
+
+// class AppButtonWithLoading extends StatelessWidget {
+//   const AppButtonWithLoading({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: onClick,
+//       child: Container(
+//           width: 100, height: 50,
+//           padding: EdgeInsets.all(10),
+//           decoration: BoxDecoration(
+//             color: AppColors.green,
+//             borderRadius: BorderRadius.circular(10),
+//           ),
+//           child: isLoading
+//               ? SpinKitWave(
+//             color: Colors.white,
+//             size: 20.0,
+//           )
+//               : Center(
+//             child: Text("Login",
+//               style: TextStyle(
+//                 fontSize: 13,
+//                 color: AppColors.white,
+//                 fontWeight: FontWeight.w600,
+//               ),
+//             ),
+//           )
+//       ),
+//     );
+//   }
+// }
+
